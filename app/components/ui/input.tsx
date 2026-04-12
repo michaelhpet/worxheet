7673ad938c04f68de30cedd6766b5040b7df5019
Michael Peter <michaelhpet@gmail.com>
2026-04-12 17:06:05 +0100

    feat: update shadcn preset

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -1,9 +1,8 @@
-import * as React from "react";
 import { Input as InputPrimitive } from "@base-ui/react/input";
+import type { ComponentProps } from "react";
+import { cn } from "@/lib/utils";
 
-import { cn } from "app/lib/utils";
-
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+function Input({ className, type, ...props }: ComponentProps<"input">) {
 	return (
 		<InputPrimitive
 			type={type}
